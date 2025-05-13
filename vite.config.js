e313e0edcd759abc22a402613329c8475fb94459
Chad Payne <cpayne@homewoodhealth.com>
2025-05-13 10:18:42 -0600

    Fixed entry point after moving it into the components directory

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-    plugins: [vue()],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/HighchartWrapper.vue'),
-            name: 'HighchartWrapper',
-            fileName: (format) => `HighchartWrapper.${format}.js`
-        },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                globals: {
-                    vue: 'Vue'
-                }
-            }
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/HighchartWrapper.vue'),
+      name: 'HighchartWrapper',
+      fileName: (format) => `HighchartWrapper.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
         }
+      }
     }
+  }
 })
